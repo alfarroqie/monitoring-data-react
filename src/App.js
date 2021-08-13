e7fa05a4b898889d34316e77d55007b63918e679
alfarroqie <48423485+alfarroqie@users.noreply.github.com>
2021-08-13 15:53:58 +0700

    task table and trend chart

--- src/App.js
+++ src/App.js
@@ -1,24 +1,112 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
+import { Table, Space, Button, Modal } from 'antd';
 import './App.css';
 
+import dataDummy from './data/dataQualityDummy.json';
+import Chart from './chart/chart.js'
+
 function App() {
+  const [isModalVisible, setIsModalVisible] = useState(false);
+  const [modalKey, setModalKey] = useState("1");
+      
+  function showModal(id){
+    setModalKey(id);
+    setIsModalVisible(true);
+  };
+      
+  const handleOk = () => {
+    setIsModalVisible(false);
+  };
+      
+  const handleCancel = () => {
+    setIsModalVisible(false);
+  };
+
+  const columns = [
+    {
+      title: 'Date',
+      dataIndex: 'date',
+      key: 'date',
+      render: ((date) => date.substr(0,10)),
+    },
+    {
+      title: 'Group',
+      dataIndex: 'group',
+      key: 'group',
+    },
+    {
+      title: 'Source',
+      dataIndex: 'source',
+      key: 'source',
+    },
+    {
+      title: 'PIC',
+      dataIndex: 'pic',
+      key: 'pic',
+    },
+    {
+      title: 'Dependents',
+      dataIndex: 'dependents',
+      key: 'dependents',
+    },
+    {
+      title: 'KPI',
+      dataIndex: 'kpi',
+      key: 'kpi',
+    },
+    {
+      title: 'Current Value',
+      dataIndex: 'current_value',
+      key: 'current_value',
+    },
+    {
+      title: 'Treshold',
+      dataIndex: 'treshold',
+      key: 'treshold',
+    },
+    {
+      title: 'Status',
+      dataIndex: 'status',
+      key: 'status',
+    },
+    {
+      title: 'Remark',
+      dataIndex: 'remark',
+      key: 'remark',
+    },
+    {
+      title: 'Aging',
+      dataIndex: 'aging',
+      key: 'aging',
+    },
+    {
+      title: 'Actions',
+      dataIndex: '_id',
+      key: '_id',
+      render: (text, record) => (
+        <Space size="middle">
+          <Button type="link" onClick={() => showModal(record._id)}>Last 30 Days</Button>
+        </Space>
+      ),
+    },
+  ]
   return (
+    <>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Table columns={columns} dataSource={dataDummy}/>
     </div>
+    <Modal 
+      title="Basic Modal" 
+      visible={isModalVisible} 
+      onOk={handleOk} 
+      onCancel={handleCancel} 
+      width={1200}>
+      <div>
+        id : {modalKey}
+      </div>
+      <Chart />
+    </Modal>
+    </>
   );
 }
 
---
+++ src/chart/chart.js
@@ -0,0 +1,88 @@
+import React, {useRef} from 'react'  
+import * as am4core from "@amcharts/amcharts4/core";  
+import * as am4charts from "@amcharts/amcharts4/charts";  
+import am4themes_animated from "@amcharts/amcharts4/themes/animated";  
+
+import chardData from '../data/chartDummy.json'  
+import { useLayoutEffect } from "react";
+  
+function Chart() {
+    const c = useRef(null);
+    useLayoutEffect(() => {
+        am4core.useTheme(am4themes_animated);  
+        let chart = am4core.create("DataChart", am4charts.XYChart);
+        
+        //filter 30 days
+        const date = new Date();
+        console.log(date)
+        const year = date.getFullYear();
+        const month = date.getMonth()+1;
+        console.log(month)
+        const lastMonth = ((month-1) >= 10 ? (month-1) : "0"+(month-1));
+        const day = date.getDate();  
+        const lastDate = year+"-"+lastMonth+"-"+day;
+        const dataFilter = chardData.filter(data => data.date > lastDate);
+        console.log(lastDate)
+
+        // Add data to chart  
+        chart.data = dataFilter;
+
+        //formatting date
+        chart.dateFormatter.inputDateFormat = "yyyy-MM-dd";
+
+        //Create Axis
+        var dateAxis = chart.xAxes.push(new am4charts.DateAxis());
+        dateAxis.dataFields.category = "date";
+        dateAxis.title.text = "Week";
+
+        var valueAxis = chart.yAxes.push(new am4charts.ValueAxis());
+        valueAxis.title.text = "PB";
+
+        //make interval Axis per week
+        dateAxis.dateFormats.setKey("week", "ww");
+        dateAxis.gridIntervals.setAll([
+            { timeUnit: "day", count: 1 },
+            { timeUnit: "day", count: 7 },
+        ]);
+
+        //create Series
+        var series = chart.series.push(new am4charts.LineSeries());
+        series.dataFields.valueY = "value";
+        series.dataFields.dateX = "date";
+        series.name = "TEST";
+        series.strokeWidth = 1;
+        
+        //Make Label Bullet
+        var labelBullet = series.bullets.push(new am4charts.LabelBullet());
+        labelBullet.label.text = "{valueY}";
+
+        // Make a panning cursor
+        chart.cursor = new am4charts.XYCursor();
+        chart.cursor.behavior = "panXY";
+        chart.cursor.xAxis = dateAxis;
+        chart.cursor.snapToSeries = series;
+
+        // // Create vertical scrollbar and place it before the value axis
+        // chart.scrollbarY = new am4core.Scrollbar();
+        // chart.scrollbarY.parent = chart.leftAxesContainer;
+        // chart.scrollbarY.toBack();
+
+        // Create a horizontal scrollbar with previe and place it underneath the date axis
+        chart.scrollbarX = new am4charts.XYChartScrollbar();
+        chart.scrollbarX.series.push(series);
+        chart.scrollbarX.parent = chart.bottomAxesContainer;
+
+        c.current = chart;
+
+        return () => {
+            chart.dispose();
+        };
+
+    }, [])
+
+    return (
+        <div id="DataChart" style={{ width: "100%", height: "500px" }}></div>
+    )
+}
+
+export default Chart
